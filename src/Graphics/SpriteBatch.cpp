60ee9cc6c99aa5983d9e21ef0f04c1681ab5a80e
JamzDev <82174996+Jamz25@users.noreply.github.com>
2025-04-10 14:31:10 +0100

    fixed spritebatch color typo

--- src/Graphics/SpriteBatch.cpp
+++ src/Graphics/SpriteBatch.cpp
@@ -84,7 +84,7 @@ void pl::SpriteBatch::draw(RenderTarget& window, const DrawData& drawData)
     vertices[2].textureUV = vertices[0].textureUV + Vector2f(drawData.textureRect.width, drawData.textureRect.height);
     vertices[3].textureUV = vertices[0].textureUV + Vector2f(0, drawData.textureRect.height);
 
-    Color colorNormalised = drawData.colour.normalise();
+    Color colorNormalised = drawData.color.normalise();
     for (int i = 0; i < 4; i++)
     {
         vertices[i].color = colorNormalised;
