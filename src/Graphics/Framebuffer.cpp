af8a0bf3fe437b4fd071473bcecee4497a8b02e3
JamzDev <82174996+Jamz25@users.noreply.github.com>
2025-04-13 18:49:22 +0100

    improved framebuffer

--- src/Graphics/Framebuffer.cpp
+++ src/Graphics/Framebuffer.cpp
@@ -25,9 +25,10 @@ bool pl::Framebuffer::create(uint32_t width, uint32_t height)
     else
     {
         glGenFramebuffers(1, &framebuffer);
-        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
     }
-
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    
     if (texture.getID() == 0)
     {
         GLuint textureColorBuffer;
@@ -37,20 +38,20 @@ bool pl::Framebuffer::create(uint32_t width, uint32_t height)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorBuffer, 0);
         
-        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-        {
-            return false;
-        }
-
         texture.setFromAllocated(textureColorBuffer, width, height);
     }
     else
     {
         texture.use();
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    }
+
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture.getID(), 0);
+    
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        return false;
     }
 
     return true;
